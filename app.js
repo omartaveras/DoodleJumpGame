cbee0a8a4386ca4821c95dffe37ed7be3e439ce2
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2020-09-29 17:16:54 +0100

    Doodle V0.1

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DomContentLoaded', ()=>{
+    const grid = document.querySelector('.grid');
+    const doodler = document.createElement('.div');
+    let doodlerLeftSpace = 50;
+    let doodlerBottomSpace = 150;
+
+    function createdDoodler(){
+        grid.appendChild(doodler);
+        doodler.classList.add('doodler');
+        doodler.style.left = doodlerLeftSpace + 'px';
+        doodler.style.bottom = doodlerBottomSpace + 'px';
+    }
+    createDoodler();
+});
