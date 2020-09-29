a12bf85f065211329286e2cf6436785421ea24c9
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2020-09-29 17:25:21 +0100

    Doodle V0.1

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-document.addEventListener('DomContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
     const grid = document.querySelector('.grid');
-    const doodler = document.createElement('.div');
+    const doodler = document.createElement('div');
     let doodlerLeftSpace = 50;
     let doodlerBottomSpace = 150;
 
-    function createdDoodler(){
+    function createDoodler(){
         grid.appendChild(doodler);
         doodler.classList.add('doodler');
         doodler.style.left = doodlerLeftSpace + 'px';
